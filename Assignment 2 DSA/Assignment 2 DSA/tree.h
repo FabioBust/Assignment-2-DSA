3dc6bcc2a3fe9102d3f3fae00b30325c16c03fb3
Fabio <f.bustamanteromillo@gmail.com>
2026-03-22 23:30:25 -0400

    Add struct Node to tree.h and all the functions declarations

--- Assignment 2 DSA/Assignment 2 DSA/tree.h
+++ Assignment 2 DSA/Assignment 2 DSA/tree.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef TREE_H
+#define TREE_H
+
+// Define structure of a tree Node
+typedef struct Node {
+    char data;
+    struct Node* left;  // Pointer to left child
+    struct Node* right; // Pointer to right child
+} Node;
+
+// Function declarations
+Node* insert(Node* root, char data);
+Node* search(Node* root, char data);
+int countNodes(Node* root);
+int treeHeight(Node* root);
+void printTree(Node* root);
+
+#endif
